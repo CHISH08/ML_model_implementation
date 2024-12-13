b59bb4a0d17e8a91047d0020b8ae7aef695ad966
CHISH08 <den.degtyarev.00@gmail.com>
2024-12-13 03:58:30 +0300

    Add random forest implementation

--- implementation/__init__.py
+++ implementation/__init__.py
@@ -1,3 +1,4 @@
 from .KNN import MyKNeighborsRegressor, MyKNeighborsClassifier
 from .Regressor import LinearRegressor, LogisticRegressor
 from .DecTree import DecisionTreeClassifier, DecisionTreeRegressor
+from .RandForest import RandomForestClassifier, RandomForestRegressor
